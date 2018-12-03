0b839533ad40b099bc15455a922314eb1650cb64
Branko Šuligoj <branko.suligoj@mobik-ics.com>
2018-12-03 15:27:11 +0100

    Finished 1, started 2

---
+++ 2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func line(id string) (bool, bool) {
+	return true, true
+}
+
+func main() {
+	f, err := os.Open("./input/input.txt")
+	if err != nil {
+		fmt.Println("there is error")
+		return
+	}
+
+	scanner := bufio.NewScanner(f)
+	frequency := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		offset, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println("There was an error")
+		}
+		frequency += offset
+	}
+
+	fmt.Println("Final frequency:", frequency)
+}
